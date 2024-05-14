3eb59f2c02fcf5fa6c1265003261e1e2350bf1f2
i7eo <admin@i7eo.com>
2024-05-14 10:13:29 +0800

    chore: setup branch

--- apps/document/.vitepress/config.ts
+++ apps/document/.vitepress/config.ts
@@ -2,18 +2,6 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  vite: {
-    resolve: {
-      conditions: ["dev"],
-      alias: {
-        "@mvbri7eo/node-gitlab": "../../../packages.node/gitlab",
-        "@mvbri7eo/node-utils": "../../../packages.node/utils",
-        "@mvbri7eo/meta": "../../../packages.web/meta",
-        "@mvbri7eo/metas": "../../../packages.web/metas",
-        "@mvbri7eo/utils": "../../../packages.web/utils",
-      },
-    },
-  },
   title: "Document",
   description: "monorepo test",
   themeConfig: {
